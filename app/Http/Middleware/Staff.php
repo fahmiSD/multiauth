94da650376376845219bf6abca6067deb868041c
Fahmi Sabila Dinnulhaq <fahmisabiladinnulhaq@gmail.com>
2023-04-13 11:19:12 +0700

    first push

---
+++ app/Http/Middleware/Staff.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Staff
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!Auth::check()){
+            return redirect('/login');
+        }
+
+        $user = Auth::user();
+        
+        if($user->role == 4){
+            return $next($request);
+        }else if($user->role == 2){
+            return redirect('/admin');
+        }else if($user->role == 3){
+            return redirect('/depthead');
+        }else if($user->role == 1){
+            return redirect('/staffsuperadmin');
+        }else if($user->role == 5){
+            return redirect('/client');
+        }
+    }
+}
---
+++ app/Http/Middleware/Superadmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Superadmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!Auth::check()){
+            return redirect('/login');
+        }
+
+        $user = Auth::user();
+        
+        if($user->role == 1){
+            return $next($request);
+        }else if($user->role == 2){
+            return redirect('/admin');
+        }else if($user->role == 3){
+            return redirect('/depthead');
+        }else if($user->role == 4){
+            return redirect('/staff');
+        }else if($user->role == 5){
+            return redirect('/client');
+        }
+       
+    }
+}
